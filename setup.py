b4324993b9ee6dd09d12bc0e234b4bacfcf9fdf3
Pablo Garcia de los Salmones <sansalinas@gmail.com>
2019-12-10 16:32:39 +0100

    Include setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='coverage-context-report',
+    author='Pablo Garcia de los Salmones',
+    version='0.0.0',
+    keywords='coverage context unittest',
+    packages=['app', 'code_demo', 'test'],
+    description='Python project to include context into coverage html report',
+    license='Apache Software License',
+    install_requires=['coverage==5.0a7', 'pytest-cov==2.8.1']
+)
